1c4351b36343afba6842848df99624dddd4f776c
Mayas Haddad <mayas_91@hotmail.com>
2015-11-20 22:04:50 +0100

    add the client through the server

--- persistClientServer.js
+++ persistClientServer.js
@@ -1,12 +1,14 @@
 var express = require('express'),
-	bodyParser = require('body-parser')
-
+	bodyParser = require('body-parser'),
+	ClientDAO = require('./lib/ClientDAO')
 var app = express()
 // parse text/json
 app.use(bodyParser.json())
 
 app.post('/add-new-lead', function(req, res) {
-	
+	ClientDAO.addClient(req.body, function(pResult) {
+		res.json(pResult)
+	})
 })
 
 app.post('/update-lead', function(req, res) {
