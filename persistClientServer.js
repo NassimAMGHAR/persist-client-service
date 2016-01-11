9e3c4866d73070a6ab1e7c5399dddbdee9f49459
Mayas Haddad <mayas_91@hotmail.com>
2016-01-11 20:22:05 +0100

    allow * cross origin

--- persistClientServer.js
+++ persistClientServer.js
@@ -6,18 +6,25 @@ var app = express()
 app.use(bodyParser.json())
 
 app.post('/add-new-client', function(req, res) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
 	ClientDAO.addClient(req.body, function(pResult) {
 		res.json(pResult)
 	})
 })
 
 app.post('/update-client', function(req, res) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
 	ClientDAO.updateClient(req.body, function(pResult) {
 		res.json(pResult)
 	})
 })
 
 app.get('/get-clients', function(req, res) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
 	ClientDAO.getClients(req.query, function(pResult) {
 		res.json(pResult)
 	})
