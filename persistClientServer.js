30e71bf422db496d896b1ea06ee04b5debfa4f7e
Mayas Haddad <mayas_91@hotmail.com>
2016-01-28 21:15:44 +0100

    add create and update purchase api and and initialize sequences at start

--- persistClientServer.js
+++ persistClientServer.js
@@ -1,15 +1,25 @@
 var express = require('express'),
 	bodyParser = require('body-parser'),
-	ClientDAO = require('./lib/ClientDAO')
+	ClientDAO = require('./lib/ClientDAO'),
+    PurchaseDAO = require('./lib/PurchaseDAO')
 var app = express()
 // parse text/json
 app.use(bodyParser.json())
+PurchaseDAO.initPurchaseSequence()
 
 app.post('/add-new-client', function(req, res) {
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
 
 	ClientDAO.addClient(req.body, function(pResult) {
+		res.json(pResult)
+	})
+})
+
+app.post('/add-or-update-purchases', function(req, res) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+	PurchaseDAO.addPurchases(req.body, function(pResult) {
 		res.json(pResult)
 	})
 })
@@ -26,6 +36,14 @@ app.get('/get-clients', function(req, res) {
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
 	ClientDAO.getClients(req.query, function(pResult) {
+		res.json(pResult)
+	})
+})
+
+app.get('/get-purchases', function(req, res) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+	PurchaseDAO.getPurchases(req.query, function(pResult) {
 		res.json(pResult)
 	})
 })
