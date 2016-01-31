82c823589368f5172aac5e883394dba9b3612b62
Mayas Haddad <mayas_91@hotmail.com>
2016-01-31 13:17:45 +0100

    fix cross origin allow

--- persistClientServer.js
+++ persistClientServer.js
@@ -3,13 +3,22 @@ var express = require('express'),
 	ClientDAO = require('./lib/ClientDAO'),
     PurchaseDAO = require('./lib/PurchaseDAO')
 var app = express()
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 // parse text/json
 app.use(bodyParser.json())
+app.use(allowCrossDomain)
 PurchaseDAO.initPurchaseSequence()
 
 app.post('/add-new-client', function(req, res) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
 
 	ClientDAO.addClient(req.body, function(pResult) {
 		res.json(pResult)
@@ -17,32 +26,24 @@ app.post('/add-new-client', function(req, res) {
 })
 
 app.post('/add-or-update-purchases', function(req, res) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
 	PurchaseDAO.addPurchases(req.body, function(pResult) {
 		res.json(pResult)
 	})
 })
 
 app.post('/update-client', function(req, res) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
 	ClientDAO.updateClient(req.body, function(pResult) {
 		res.json(pResult)
 	})
 })
 
 app.get('/get-clients', function(req, res) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
 	ClientDAO.getClients(req.query, function(pResult) {
 		res.json(pResult)
 	})
 })
 
 app.get('/get-purchases', function(req, res) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
 	PurchaseDAO.getPurchases(req.query, function(pResult) {
 		res.json(pResult)
 	})
