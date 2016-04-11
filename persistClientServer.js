8ba3632ab3bed26c13cbcec3c6bb6f262d875c1c
Mayas Haddad <mayas_91@hotmail.com>
2016-04-11 13:42:41 +0200

    add bunyan and log errors

--- persistClientServer.js
+++ persistClientServer.js
@@ -1,16 +1,16 @@
-var express = require('express'),
-	bodyParser = require('body-parser'),
-	ClientDAO = require('./lib/ClientDAO'),
-    PurchaseDAO = require('./lib/PurchaseDAO')
+var express = require('express')
+var bodyParser = require('body-parser')
+var ClientDAO = require('./lib/ClientDAO')
+var PurchaseDAO = require('./lib/PurchaseDAO')
 var app = express()
 
-//CORS middleware
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+// CORS middleware
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
 
-    next();
+  next()
 }
 
 // parse text/json
@@ -18,40 +18,39 @@ app.use(bodyParser.json())
 app.use(allowCrossDomain)
 PurchaseDAO.initPurchaseSequence()
 
-app.post('/add-new-client', function(req, res) {
-
-	ClientDAO.addClient(req.body, function(pResult) {
-		res.json(pResult)
-	})
+app.post('/add-new-client', function (req, res) {
+  ClientDAO.addClient(req.body, function (pResult) {
+    res.json(pResult)
+  })
 })
 
-app.post('/add-or-update-purchases', function(req, res) {
-	PurchaseDAO.addPurchases(req.body, function(pResult) {
-		res.json(pResult)
-	})
+app.post('/add-or-update-purchases', function (req, res) {
+  PurchaseDAO.addPurchases(req.body, function (pResult) {
+    res.json(pResult)
+  })
 })
 
-app.post('/update-client', function(req, res) {
-	ClientDAO.updateClient(req.body, function(pResult) {
-		res.json(pResult)
-	})
+app.post('/update-client', function (req, res) {
+  ClientDAO.updateClient(req.body, function (pResult) {
+    res.json(pResult)
+  })
 })
 
-app.get('/get-clients', function(req, res) {
-	ClientDAO.getClients(req.query, function(pResult) {
-		res.json(pResult)
-	})
+app.get('/get-clients', function (req, res) {
+  ClientDAO.getClients(req.query, function (pResult) {
+    res.json(pResult)
+  })
 })
 
-app.get('/get-top-n-clients', function(req, res) {
-	ClientDAO.getTopNClients(req.query, function(pResult) {
-		res.json(pResult)
-	})
+app.get('/get-top-n-clients', function (req, res) {
+  ClientDAO.getTopNClients(req.query, function (pResult) {
+    res.json(pResult)
+  })
 })
 
-app.get('/get-purchases', function(req, res) {
-	PurchaseDAO.getPurchases(req.query, function(pResult) {
-		res.json(pResult)
-	})
+app.get('/get-purchases', function (req, res) {
+  PurchaseDAO.getPurchases(req.query, function (pResult) {
+    res.json(pResult)
+  })
 })
 app.listen(3000)
