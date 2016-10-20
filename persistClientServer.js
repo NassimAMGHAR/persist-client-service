ef1913d9000683890610b20cba81f9b711d4c630
Mayas <mayas.haddad@save.co>
2016-10-20 23:33:53 +0200

    refactor: switch to RESTful verbs

--- persistClientServer.js
+++ persistClientServer.js
@@ -18,39 +18,40 @@ app.use(bodyParser.json())
 app.use(allowCrossDomain)
 PurchaseDAO.initPurchaseSequence()
 
-app.post('/add-new-client', function (req, res) {
+app.put('/clients', function (req, res) {
   ClientDAO.addClient(req.body, function (pResult) {
     res.json(pResult)
   })
 })
 
-app.post('/add-or-update-purchases', function (req, res) {
-  PurchaseDAO.addPurchases(req.body, function (pResult) {
-    res.json(pResult)
-  })
-})
-
-app.post('/update-client', function (req, res) {
+app.post('/clients', function (req, res) {
   ClientDAO.updateClient(req.body, function (pResult) {
     res.json(pResult)
   })
 })
 
-app.get('/get-clients', function (req, res) {
+app.get('/clients', function (req, res) {
   ClientDAO.getClients(req.query, function (pResult) {
     res.json(pResult)
   })
 })
 
-app.get('/get-top-n-clients', function (req, res) {
+app.get('/clients/top', function (req, res) {
   ClientDAO.getTopNClients(req.query, function (pResult) {
     res.json(pResult)
   })
 })
 
-app.get('/get-purchases', function (req, res) {
+app.post('/purchases', function (req, res) {
+  PurchaseDAO.addPurchases(req.body, function (pResult) {
+    res.json(pResult)
+  })
+})
+
+app.get('/purchases', function (req, res) {
   PurchaseDAO.getPurchases(req.query, function (pResult) {
     res.json(pResult)
   })
 })
+
 app.listen(3000)
